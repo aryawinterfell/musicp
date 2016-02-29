8ae4b14c66de4d83f040dd6c37bcc1571eb22e48
adriana <adrianamolello@gmail.com>
2016-02-29 23:51:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musicp::Application.config.secret_key_base = '359066016b6b15f80662b25e5cbde818e9af844b980edf73759e6bcbae48288c5daaa64f433ae584bf1d32a6691989e7f4d5afdb6ec9842519b9ced2c121726d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Musicp::Application.config.session_store :cookie_store, key: '_musicp_session'
