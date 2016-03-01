55153bc0ec0c2ad17768be3de1d062e9015e2ef3
adriana <adrianamolello@gmail.com>
2016-03-01 00:14:30 +0000

    Added log model

---
+++ db/migrate/20160301001045_create_logs.rb
@@ -0,0 +1,13 @@
+class CreateLogs < ActiveRecord::Migration
+  def change
+    create_table :logs do |t|
+
+    	t.date :date
+    	t.string :piece
+    	t.time :time
+    	t.text :observations
+
+      t.timestamps
+    end
+  end
+end
